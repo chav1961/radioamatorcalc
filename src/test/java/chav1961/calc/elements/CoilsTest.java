8c80cc467ed0e82b615ec29359208c3128b0c633
achernomyrdin <achernomyrdin@Chernomirdin-PC.cp.compassplus.ru>
2018-09-26 17:14:57 +0500

    Daily commit

--- src/test/java/chav1961/calc/elements/CoilsTest.java
+++ src/test/java/chav1961/calc/elements/CoilsTest.java
@@ -2,8 +2,10 @@ package chav1961.calc.elements;
 
 import java.io.IOException;
 
+import org.junit.Assert;
 import org.junit.Test;
 
+import chav1961.calc.elements.coils.CoilsCalculationType;
 import chav1961.calc.elements.coils.singlecoilsplugin.SingleCoilsService;
 import chav1961.calc.interfaces.PluginInterface;
 import chav1961.calc.interfaces.PluginInterface.PluginInstance;
@@ -14,7 +16,9 @@ import chav1961.purelib.basic.exceptions.SyntaxException;
 import chav1961.purelib.basic.interfaces.LoggerFacade;
 import chav1961.purelib.i18n.PureLibLocalizer;
 import chav1961.purelib.i18n.interfaces.Localizer;
+import chav1961.purelib.ui.interfacers.FormManager;
 import chav1961.purelib.ui.interfacers.FormManager.RefreshMode;
+import chav1961.purelib.ui.swing.AutoBuiltForm;
 import util.PluginTestingUtil;
 
 public class CoilsTest {
@@ -26,10 +30,44 @@ public class CoilsTest {
 		
 		try(final Localizer			parent = new PureLibLocalizer();
 			final LoggerFacade		logger = new NullLoggerFacade()) {
-			
 			final PluginInstance	inst = pi.newInstance(parent, logger);
 			
-			PluginTestingUtil.playScenario(inst,new PluginTestingUtil.AssignValue("",,RefreshMode. awaited));
+			Assert.assertTrue(PluginTestingUtil.playScenario((FormManager<?,?>)((AutoBuiltForm<?>)inst).getFormManagerAssociated()
+					,new PluginTestingUtil.AssignValue("length",10.0f,RefreshMode.RECORD_ONLY)
+					,new PluginTestingUtil.AssignValue("diameter",10.0f,RefreshMode.RECORD_ONLY)
+					,new PluginTestingUtil.AssignValue("wireDiameter",0.1f,RefreshMode.RECORD_ONLY)
+					,new PluginTestingUtil.AssignValue("calcType",CoilsCalculationType.INDUCTANCE,RefreshMode.RECORD_ONLY)
+					,new PluginTestingUtil.AssignValue("coils",10,RefreshMode.RECORD_ONLY)
+					,new PluginTestingUtil.ProcessAction("calculate",null,RefreshMode.RECORD_ONLY)
+					,new PluginTestingUtil.CompareValue("inductance",10,0.01)
+					));
+			Assert.assertTrue(PluginTestingUtil.playScenario((FormManager<?,?>)inst
+					,new PluginTestingUtil.AssignValue("length",10.0f,RefreshMode.RECORD_ONLY)
+					,new PluginTestingUtil.AssignValue("diameter",10.0f,RefreshMode.RECORD_ONLY)
+					,new PluginTestingUtil.AssignValue("wireDiameter",0.1f,RefreshMode.RECORD_ONLY)
+					,new PluginTestingUtil.AssignValue("calcType",CoilsCalculationType.NUMBER_OF_COILS,RefreshMode.RECORD_ONLY)
+					,new PluginTestingUtil.AssignValue("inductance",10,RefreshMode.RECORD_ONLY)
+					,new PluginTestingUtil.ProcessAction("calculate",null,RefreshMode.RECORD_ONLY)
+					,new PluginTestingUtil.CompareValue("coils",10,0.01)
+					));
+			
+			Assert.assertFalse(PluginTestingUtil.playScenario((FormManager<?,?>)inst
+					,new PluginTestingUtil.AssignValue("length",0.0f,RefreshMode.RECORD_ONLY)
+					));
+			Assert.assertFalse(PluginTestingUtil.playScenario((FormManager<?,?>)inst
+					,new PluginTestingUtil.AssignValue("diameter",0.0f,RefreshMode.RECORD_ONLY)
+					));
+			Assert.assertFalse(PluginTestingUtil.playScenario((FormManager<?,?>)inst
+					,new PluginTestingUtil.AssignValue("wireDiameter",0.0f,RefreshMode.RECORD_ONLY)
+					));
+			Assert.assertFalse(PluginTestingUtil.playScenario((FormManager<?,?>)inst
+					,new PluginTestingUtil.AssignValue("calcType",CoilsCalculationType.NUMBER_OF_COILS,RefreshMode.RECORD_ONLY)
+					,new PluginTestingUtil.AssignValue("coils",0,RefreshMode.RECORD_ONLY)
+					));
+			Assert.assertFalse(PluginTestingUtil.playScenario((FormManager<?,?>)inst
+					,new PluginTestingUtil.AssignValue("calcType",CoilsCalculationType.INDUCTANCE,RefreshMode.RECORD_ONLY)
+					,new PluginTestingUtil.AssignValue("inductance",0.0f,RefreshMode.RECORD_ONLY)
+					));
 		}
 	}
 }
--- src/test/java/util/PluginTestingUtil.java
+++ src/test/java/util/PluginTestingUtil.java
@@ -1,8 +1,11 @@
 package util;
 
+import java.lang.reflect.Field;
+
 import org.junit.Assert;
 
 import chav1961.calc.interfaces.PluginInterface;
+import chav1961.purelib.basic.exceptions.FlowException;
 import chav1961.purelib.basic.exceptions.LocalizationException;
 import chav1961.purelib.i18n.PureLibLocalizer;
 import chav1961.purelib.i18n.interfaces.Localizer;
@@ -38,8 +41,21 @@ public class PluginTestingUtil {
 		} 
 	}
 
-	public static void playScenario(final FormManager<?,?> instance, final ScenarioStep... steps) {
-		
+	public static boolean playScenario(final FormManager<?,?> instance, final ScenarioStep... steps) {
+		if (instance == null) {
+			throw new IllegalArgumentException("Instance can't be null");
+		}
+		else if (steps == null || steps.length == 0) {
+			throw new IllegalArgumentException("Scenario step list can't be null or empty array");
+		}
+		else {
+			for (ScenarioStep item : steps) {
+				if (!item.execute(instance)) {
+					return false;
+				}
+			}
+			return true;
+		}
 	}
 
 	private static void checkStringArray(final String[] array) {
@@ -51,44 +67,161 @@ public class PluginTestingUtil {
 		}
 	}
 	
-	
 	public static abstract class ScenarioStep {
-		public abstract boolean execute(final FormManager<?,?> instance);
+		public abstract boolean execute(final FormManager instance);
 	}
 	
-	public static class ReadValue extends ScenarioStep {
-		public ReadValue(final int fieldName, final Object fieldValue) {
-			
+	public static class CompareValue extends ScenarioStep {
+		private final String	fieldName;
+		private final Object	awaitedValue;
+		private final double	precision;
+		
+		public CompareValue(final String fieldName, final Object awaitedValue) {
+			this(fieldName,awaitedValue,0.0);
 		}
 
+		public CompareValue(final String fieldName, final Object awaitedValue, final double precision) {
+			if (fieldName == null || fieldName.isEmpty()) {
+				throw new IllegalArgumentException("Field name ca't be null or empty");
+			}
+			else if (precision < 0) {
+				throw new IllegalArgumentException("Precision to test can't be negative");
+			}
+			else {
+				this.fieldName = fieldName;
+				this.awaitedValue = awaitedValue;
+				this.precision = precision;
+			}
+		}
+		
 		@Override
-		public boolean execute(final FormManager<?, ?> instance) {
-			// TODO Auto-generated method stub
-			return false;
+		public boolean execute(final FormManager instance) {
+			if (instance == null) {
+				throw new NullPointerException("Instance can't be null"); 
+			}
+			else {
+				try{final Class<FormManager<?, ?>>	clazz = (Class<FormManager<?, ?>>) instance.getClass();
+					final Field						f = clazz.getDeclaredField(fieldName);
+					
+					f.setAccessible(true);
+					final Object					value = f.get(instance);
+					final double					value1, value2;
+					boolean							usePrecision1 = false, usePrecision2 = false;
+
+					if (value instanceof Float) {
+						value1 = ((Float)value).doubleValue();
+						usePrecision1 = true;
+					}
+					else if (value instanceof Double) {
+						value1 = ((Double)value).doubleValue();
+						usePrecision1 = true;
+					}
+					else {
+						value1 = 0;
+					}
+					if (awaitedValue instanceof Float) {
+						value2 = ((Float)awaitedValue).doubleValue();
+						usePrecision2 = true;
+					}
+					else if (awaitedValue instanceof Double) {
+						value2 = ((Double)awaitedValue).doubleValue();
+						usePrecision2 = true;
+					}
+					else {
+						value2 = 0;
+					}
+					if (usePrecision1 && usePrecision2) {
+						return Math.abs(value1 - value2) < precision;
+					}
+					else {
+						return value == awaitedValue || value != null && value.equals(awaitedValue);
+					}
+				} catch (NoSuchFieldException | SecurityException | IllegalAccessException e) {
+					throw new IllegalArgumentException(e);
+				}
+			}
 		}
 	}
-
+	
 	public static class AssignValue extends ScenarioStep {
-		public AssignValue(final int fieldName, final Object fieldValue, final RefreshMode... awaited) {
-			
+		private final String 		fieldName;
+		private final Object 		fieldValue;
+		private final RefreshMode[]	awaited;
+		
+		public AssignValue(final String fieldName, final Object fieldValue, final RefreshMode... awaited) {
+			if (fieldName == null || fieldName.isEmpty()) {
+				throw new IllegalArgumentException("Field name ca't be null or empty");
+			}
+			else if (awaited == null || awaited.length == 0) {
+				throw new IllegalArgumentException("Awaited refresh mode can't be null or empty");
+			}
+			else {
+				this.fieldName = fieldName;
+				this.fieldValue = fieldValue;
+				this.awaited = awaited;
+			}
 		}
 
 		@Override
-		public boolean execute(final FormManager<?, ?> instance) {
-			// TODO Auto-generated method stub
-			return false;
+		public boolean execute(final FormManager instance) {
+			if (instance == null) {
+				throw new NullPointerException("Instance can't be null"); 
+			}
+			else {
+				try{final Class<FormManager<?, ?>>	clazz = (Class<FormManager<?, ?>>) instance.getClass();
+					final Field						f = clazz.getDeclaredField(fieldName);
+					
+					f.setAccessible(true);
+					final Object					oldValue = f.get(instance);
+					
+					f.set(instance,fieldValue);
+					final RefreshMode				result = instance.onField(instance,null,fieldName,oldValue);
+					
+					for (RefreshMode item : awaited) {
+						if (result == item) {
+							return true;
+						}
+					}
+					return false;
+				} catch (NoSuchFieldException | SecurityException | IllegalAccessException | LocalizationException | FlowException e) {
+					throw new IllegalArgumentException(e);
+				}
+			}
 		}
 	}
 	
 	public static class ProcessAction extends ScenarioStep {
-		public ProcessAction(final String action, final RefreshMode awaited) {
-			
+		private final String 		action;
+		private final Object 		parameter;
+		private final RefreshMode[]	awaited;
+		
+		public ProcessAction(final String action, final Object parameter, final RefreshMode... awaited) {
+			if (action == null || action.isEmpty()) {
+				throw new IllegalArgumentException("Field name ca't be null or empty");
+			}
+			else if (awaited == null || awaited.length == 0) {
+				throw new IllegalArgumentException("Awaited refresh mode can't be null or empty");
+			}
+			else {
+				this.action = action;
+				this.parameter = parameter;
+				this.awaited = awaited;
+			}
 		}
 		
 		@Override
-		public boolean execute(FormManager<?, ?> instance) {
-			// TODO Auto-generated method stub
-			return false;
+		public boolean execute(FormManager instance) {
+			try{final RefreshMode				result = instance.onAction(instance,null,action,parameter);
+			
+				for (RefreshMode item : awaited) {
+					if (result == item) {
+						return true;
+					}
+				}
+				return false;
+			} catch (SecurityException | LocalizationException | FlowException e) {
+				throw new IllegalArgumentException(e);
+			}
 		}		
 	}
 
