6796b937ae323ceead84738184aa56ce244c77ff
chav1961 <chav1961@mail.ru>
2022-03-31 16:05:05 +0500

    Daily commit

--- src/main/java/chav1961/calc/JDIExampleDebuggee.java
+++ src/main/java/chav1961/calc/JDIExampleDebuggee.java
@@ -1,6 +1,8 @@
 package chav1961.calc;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Optional;
 
 public class JDIExampleDebuggee {
 
@@ -11,6 +13,22 @@ public class JDIExampleDebuggee {
         String jdi = "Java Debug Interface"; //add a break point here and also stepping in here
         String text = "Today, we'll dive into " + jdi;
         System.out.println(text);
+
+        System.err.println(System.getProperties());
+        
+        ProcessHandle.allProcesses().forEach(JDIExampleDebuggee::print);
+        // https://github.com/scijava/native-lib-loader
+    }
+    
+    private static void print(final ProcessHandle ph) {
+    	if (!ph.info().command().equals(Optional.empty())) {
+        	System.err.println("PID="+ph.pid());
+        	System.err.println("Cmd="+ph.info().command());
+        	Optional<String[]> args = ph.info().arguments();
+        	if (args.isPresent()) {
+            	System.err.println("Args="+Arrays.toString(args.get()));
+        	}
+    	}
     }
     
 }
